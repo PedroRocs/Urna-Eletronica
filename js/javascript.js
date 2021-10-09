1cc5d648e90cb6543f1d006643437cdbcaa13c6a
PedroRocs <pedrorochacardoso1908@gmail.com>
2021-10-09 11:58:20 -0300

    upload

--- js/javascript.js
+++ js/javascript.js
@@ -5,98 +5,111 @@ let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
 let numeros = document.querySelector('.d-1-3');
 
+
 let etapaAtual = 0;
-let numero="";
-
-function comecarEtapa(){
+let numero = "";
+let brancoo=false;
+function comecarEtapa() {
+    numero="";
+    brancoo=false;
     let etapa = etapas[etapaAtual];
-    let numeroHtml="";
+    let numeroHtml = "";
 
-    for(let i=0;i<etapa.numeros;i++){
+    for (let i = 0; i < etapa.numeros; i++) {
         numeroHtml += '<div class="numero"></div>';
-        if(i === 0 ){
-            numeroHtml='<div class = "numero pisca"></div>';
+        if (i === 0) {
+            numeroHtml = '<div class = "numero pisca"></div>';
         }
     }
 
-    
-    seuVotoPara.style.display='none';
-    cargo.innerHTML=etapa.titulo;
-    desccricao.innerHTML='';
-    aviso.style.display='none';
-    lateral.innerHTML='';
-    numeros.innerHTML=numeroHtml;
-    
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    desccricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+
 }
 
 
-function clicou(n){
+function clicou(n) {
     let elnumero = document.querySelector('.numero.pisca');
-   
-    
-    if(elnumero !== null){
+
+
+    if (elnumero !== null) {
         elnumero.innerHTML = n;
-        numero= `${numero}${n}`;
-        
+        numero = `${numero}${n}`;
+
 
         elnumero.classList.remove('pisca');
-        if(elnumero.nextElementSibling!==null){
-        elnumero.nextElementSibling.classList.add('pisca')
-        }
-        else{
+        if (elnumero.nextElementSibling !== null) {
+            elnumero.nextElementSibling.classList.add('pisca')
+        } else {
             atualizaInterface();
         }
     }
-   
+
 }
-function atualizaInterface(){
+
+function atualizaInterface() {
     let etapa = etapas[etapaAtual];
-    let candidato = etapa.candidatos.filter((item)=>{
+    let candidato = etapa.candidatos.filter((item) => {
+
+
+        if (item.numero === numero) {
+            return true
+        } else {
+            return false
+        }
+
+    });
+    if (candidato.length > 0) {
+        candidato = candidato[0]
+        seuVotoPara.style.display = 'block';
+        desccricao.innerHTML = `Candidato: ${candidato.nome}<br/> Partido: ${candidato.partido}`;
+        aviso.style.display = 'block';
+        let fotosHtml = '';
+        for (let i in candidato.fotos) {
+            fotosHtml += `  <div class="d-1-image"><img src="../imagens/${candidato.fotos[i].url}" alt=""> ${candidato.fotos[i].legenda} </div>`
+
+        }
+        lateral.innerHTML = fotosHtml;
+    } else {
+        seuVotoPara.style.display = "block";
+        aviso.style.display = 'block';
+        desccricao.innerHTML = '<div class="aviso--grande pisca">VOTO NULO <div>';
+      
+   
+        
+    }
+
+
+}
+
+
+
+
+function branco() {
     
-       
-    if(item.numero===numero){
-        return true
-    }
-    else{
-        return false
-    }
-
-});
-if(candidato.length>0){
-    candidato=candidato[0]
-    seuVotoPara.style.display='block';
-    desccricao.innerHTML=`Candidato: ${candidato.nome}<br/> Partido: ${candidato.partido}`;
-    aviso.style.display='block';
-    let fotosHtml = '';
-    for (let i in candidato.fotos) {
-        fotosHtml += `  <div class="d-1-image"><img src="../imagens/${candidato.fotos[i].url}" alt=""> ${candidato.fotos[i].legenda} </div>`
-       
-    }
-    lateral.innerHTML=fotosHtml;
-}
-else{
-    branco()
+    if(numero===""){
+        brancoo=true;
+    numeros.innerHTML="";
+    seuVotoPara.style.display = "block";
+    aviso.style.display = 'block';
+    desccricao.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO <div>';
 }
 
-
+alert("Para voltar o branco não ode ter digitado em nenhum numero")
 }
 
+function corrige() {
 
-
-
-function branco(){
-    seuVotoPara.style.display="block";
-    aviso.style.display = 'block'
-}
-
-function corrige(){
-    
     comecarEtapa();
 }
 
-function confirma(){
+function confirma() {
     alert("cicou em confirma")
 }
 
 comecarEtapa();
-
